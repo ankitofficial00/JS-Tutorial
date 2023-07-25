30a70f8479d3e75d9f18dd0ba2dc2b8d2ecc7143
ANKIT <123539463+ankitvillagercoder@users.noreply.github.com>
2023-07-25 17:41:25 +0000

    some new files added in 01_bascis folder

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=12345;
+let accountEmail="ankit@gmail.com";
+var accountPassword="234555";
+accountCity="Hyaderbad";
+let accountState;
+// accountId=345555; // not allowded
+accountEmail="rao@gmailo.com"
+accountPassword="323223232"
+accountCity="vizag";
+
+/*
+perefer not use the var keyword 
+because of issue in block scope anfd functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"  // for the newer version of javascript code
+
+// alert(3+3) // we are using node js , not browser
+let firstName="ankit"
+let age=22
+let isLoggedIn=true
+let state;
+let price=null;
+console.log(typeof price);
+console.log(typeof state);
+// Number
+// string
+//Boolean
+// bigint
+// symbol -->unique
+// object
+// null --> standlone value
+// undefined 
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null); // null is object type 
+console.log(typeof undefined); // undedined == which is language error 
