a6a1a7f76e3c99f1699d65f3845273549d4d8deb
ANKIT <123539463+ankitvillagercoder@users.noreply.github.com>
2023-07-26 16:17:04 +0000

    new file added in 01_basics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let score="33ab"
+// let score=null;
+// let score=undefined;
+// let score=true;
+let score="ankit"
+console.log(typeof score)
+// let valueInNumber=Number(score) 
+let valueInNumber=parseInt(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// 1 to true
+let isLoggedIn="6756";
+let BooleanValue=Boolean(isLoggedIn);
+console.log(BooleanValue);
+
+let someNumber=6789
+let stringNumber=String(someNumber);
+console.log(typeof stringNumber,stringNumber);
