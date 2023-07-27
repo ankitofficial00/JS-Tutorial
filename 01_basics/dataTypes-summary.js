13f71010d12c3b792b6eee0b4a4413f6bb92bb50
ANKIT <123539463+ankitvillagercoder@users.noreply.github.com>
2023-07-27 16:17:45 +0000

    datatypes summary file added

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,40 @@
+// primitive 
+// number, string,symbol,null,undefined,boolean,bigInt
+
+let score=34;
+let name="ankit";
+let isLoggedIn=true;
+let userValue;
+const scoreFloat=23.78;
+let outTemp=null;
+
+let id=Symbol('34');
+let anotherId=Symbol('34');
+console.log(id===anotherId);
+
+let bigNumber=478788899899n;
+
+// non-primitive or refernce data types
+// objects,array,functions etc.
+
+
+const subjects=["maths","science","computer","gk","physiology","botanay"];
+
+let studentDetails={
+    name:"Ankit",
+    age:22,
+    marriedStatus:true,
+    gender:"male",
+};
+
+
+let myName=function(){
+    console.log("my name is ankit");
+};
+
+console.log(myName());
+
+
+console.log(typeof score,typeof name,typeof isLoggedIn,typeof userValue,typeof id,typeof scoreFloat,typeof outTemp);
+
+console.log(typeof myName,typeof studentDetails,typeof subjects);
