865488e75741136ec38dc31946ff9a792027c42f
ANKIT <123539463+ankitvillagercoder@users.noreply.github.com>
2023-07-31 15:14:53 +0000

    new basics folder is added

---
+++ 02_basics/01_array.js
@@ -0,0 +1,16 @@
+const myarr=["ankit",'hitesh',true,false,67,89];
+const subject=["hindi","english"]
+// array methods
+// myarr.push(56);
+// myarr.pop()
+// myarr.unshift(4)
+// myarr.shift()
+// console.log(myarr.length);
+// myarr.splice(3,2,"monika","mona");
+// const newArr=myarr.slice(0,2);
+// const newArr=myarr;
+// myarr.push(subject);
+newArr= myarr.join()
+console.log(myarr);
+console.log( typeof newArr);
+
